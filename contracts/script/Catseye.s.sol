68d1b32405744123997b80fa53d26cd123215142
Adam Stox <adamstox@protonmail.com>
2024-11-24 15:34:16 -0500

    update file structure

--- contracts/script/Catseye.s.sol
+++ contracts/script/Catseye.s.sol
@@ -11,7 +11,8 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         catseye = new Catseye(oracle);
 
